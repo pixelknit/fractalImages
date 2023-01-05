7af0af09746244d20ef4b610486d89fda7a856bf
fpesantez <54825485+pixelknit@users.noreply.github.com>
2023-01-05 11:20:54 +0000

    Add files via upload

---
+++ src/BitmapFileHeader.h
@@ -0,0 +1,20 @@
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+#include <cstdint>
+
+using namespace std;
+
+#pragma pack(2)
+
+namespace felipe{
+struct BitmapFileHeader{
+    char header[2]{'B','M'};
+    int32_t fileSize;
+    int32_t reserved{0};
+    int32_t dataOffset;
+    
+};
+}
+
+
+#endif /* BITMAPFILEHEADER_H_ */
